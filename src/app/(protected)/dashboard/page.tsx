625a2d1114b33a9ccbbb9ffc105b0a800af1a755
Devansh Singh Kushwah <devanshs843@gmail.com>
2025-01-05 01:27:16 +0530

    Prisma and DB setup completed

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -4,8 +4,8 @@ type Props = {}
 
 const DashboardPage = (props: Props) => {
 
-    // Server action to onboard the user
-    // 200 || 201
+    // WIP: Server action to onboard the user
+    // WIP: 200 || 201
 
     return (
         <div>DashboardPage</div>
